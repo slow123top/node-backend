7bc3d2b673fd8091c48776945e8dd124944a827e
huyuyang <huyuyang@inspur.com>
2019-11-30 11:04:52 +0800

    优化node数据库交互

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const User = require('./database/schema');
 const querystring = require('querystring');
 const HttpResponseText = require('./utils/response');
 
+const mongoose = require('mongoose');
 const GenerateUuid = require('./utils/uuid');
 
 // 创建一个HTTP服务器R
@@ -17,8 +18,16 @@ http.createServer((req, res) => {
     // 获取请求路径  不同接口
     const reqUrl = req.url;
     if (reqUrl === '/query') {
-        // 查询数据
-        User.find({}, (err, result) => {
+        mongoose.connect('mongodb://localhost:27017/nodedb').then(
+           async (mongoose)=>{
+            let Schema = mongoose.Schema;
+             const user =  mongoose.model('User',new Schema({
+                id: { type: String },
+                name: { type: String },
+                password: { type: String },
+            }));
+            await user.find({});
+           }).then((err, result) => {
             if (err) {
                 res.write(err);
             }
@@ -26,19 +35,35 @@ http.createServer((req, res) => {
                 let responseEntity;
                 if (!result) {
                     responseEntity = new HttpResponseText('ERROR', '数据返回异常，问一下数据库管理员吧', '');
-                } else if (result && !result.length) {
-                    responseEntity = new HttpResponseText('ERROR', '获取到的数据为空', []);
                 } else {
-                    responseEntity = new HttpResponseText('SUCCESS', '获取数据成功', result);
+                    responseEntity = new HttpResponseText('SUCCESS', '获取数据成功', result || []);
                 }
                 res.write(JSON.stringify(responseEntity));
             }
             res.end();
         });
+        // 查询数据
+        // User.find({}, (err, result) => {
+        //     if (err) {
+        //         res.write(err);
+        //     }
+        //     else {
+        //         let responseEntity;
+        //         if (!result) {
+        //             responseEntity = new HttpResponseText('ERROR', '数据返回异常，问一下数据库管理员吧', '');
+        //         } else {
+        //             responseEntity = new HttpResponseText('SUCCESS', '获取数据成功', result || []);
+        //         }
+        //         res.write(JSON.stringify(responseEntity));
+        //     }
+        //     res.end();
+        // });
     } else if (reqUrl === '/insert') {
         // 插入数据
         let putData = '';
         let responeseEntity;
+
+        insertData().then();
         // 注册监听, 接收数据块
         req.on("data", function (postDataChunk) {
             putData += postDataChunk;
@@ -47,6 +72,7 @@ http.createServer((req, res) => {
         req.on("end", function () {
             const arr = querystring.parse(putData);
             arr.id = GenerateUuid();
+            console.log(arr);
             User.insertMany([arr], (error, docs) => {
                 if (error) {
                     responeseEntity = new HttpResponseText('ERROR', '插入数据失败', '');
@@ -57,7 +83,6 @@ http.createServer((req, res) => {
                 res.end();
             });
         });
-
     } else if (reqUrl === '/update') {
         User.update({ name: 'huyuyang' }, { password: '密码修改' }, (err, docs) => {
             if (err) {
@@ -97,32 +122,49 @@ http.createServer((req, res) => {
         });
         // 数据接收完毕, 执行回调函数
         req.on("end", function () {
-            res.writeHead(200, { "Content-Type": "text/plain" });
             const arr = querystring.parse(postData);
-            let ids = postData.ids;
-            if (Object.prototype.toString.call(postData.ids) === '[object Array]') {
+            let ids = arr.ids;
+            if (Object.prototype.toString.call(arr.ids) === '[object Array]') {
                 ids = arr.ids.reduce((sum, next) => {
                     sum.push({ id: next })
                     return sum;
                 }, []);
+            } else {
+                ids = [arr.ids];
             }
-
-            User.deleteMany({ id: { $in: ids } }, (err) => {
+            console.log(arr.ids);
+            let len = ids.length;
+            // ids.forEach((ele, index) => {
+            User.deleteMany({ id: { $in: arr.ids } }, (err) => {
                 if (err) {
-                    res.end('删除失败')
+                    res.write(JSON.stringify(new HttpResponseText('ERROR', err)));
+                    res.end();
                 } else {
+                    res.write(JSON.stringify(new HttpResponseText('SUCCESS', '删除成功')));
                     res.end();
                 }
             })
         });
-        // 删除数据
-        // User.deleteOne({ name: 'huyuyang' }, (err, docs) => {
-
-        // });
     } else {
 
     }
 }).listen(8080);
 
+
+
+async function insertData(){
+    await onData();
+    await endData();
+    return `完成插入数据`;
+}
+
+
+function onData(){
+    return new Promise();
+}
+
+function endData(){
+    return new Promise();
+}
 
 
--- src/utils/uuid.js
+++ src/utils/uuid.js
@@ -1,6 +1,6 @@
 function GenerateUuid() {
     const time = new Date();
-    return time.toTimeString();
+    return time.getTime();
 }
 
 module.exports = GenerateUuid;
